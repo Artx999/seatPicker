111b0107786f1e41468b0b679830725b31e69ea4
dlirA01 <dlirA01@users.noreply.github.com>
2020-09-28 00:17:24 +0200

    Changed entire CSS file

--- index.php
+++ index.php
@@ -3,19 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.0/css/line.css">
 </head>
 <body>
 <main>
     <div id="propChooser">
         <h1>How will your layout be?</h1>
         <form>
-            <label for="seatHeight">Height:</label>
-            <label for="seatWidth">Width:</label>
-            <input type="number" id="seatHeight">
-            <input type="number" id="seatWidth">
+            <div class="gridBlock">
+                <label for="seatHeight">Height:</label>
+                <input id="seatHeight">
+            </div>
+            <div class="gridBlock">
+                <!-- Empty For Now -->
+            </div>
+            <div class="gridBlock">
+                <label for="seatWidth">Width:</label>
+                <input id="seatWidth">
+            </div>
+            <div id="buttonWrapper">
+                <button>Next <i class="uil uil-angle-right-b"></i></button>
+            </div>
         </form>
     </div>
     <div id="seatChart"></div>
