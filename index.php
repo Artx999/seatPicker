f2e40efdca53c00b6c7569b652fe6036b0ca5bf7
Artx999 <54852665+Artx999@users.noreply.github.com>
2020-09-27 23:29:39 +0200

    First steps

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+<main>
+    <div id="propChooser">
+        <h1>How will your layout be?</h1>
+        <form>
+            <label for="seatHeight">Height:</label>
+            <label for="seatWidth">Width:</label>
+            <input id="seatHeight">
+            <input id="seatWidth">
+        </form>
+    </div>
+    <div id="seatChart"></div>
+</main>
+</body>
+</html>
