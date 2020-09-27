aa440f53eb29b40ed6501837c9911ab224ee7db4
Artx999 <54852665+Artx999@users.noreply.github.com>
2020-09-28 00:10:20 +0200

    Minor changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <title>Title</title>
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 </head>
 <body>
 <main>
@@ -13,11 +14,12 @@
         <form>
             <label for="seatHeight">Height:</label>
             <label for="seatWidth">Width:</label>
-            <input id="seatHeight">
-            <input id="seatWidth">
+            <input type="number" id="seatHeight">
+            <input type="number" id="seatWidth">
         </form>
     </div>
     <div id="seatChart"></div>
 </main>
 </body>
+<script src="script.js"></script>
 </html>
